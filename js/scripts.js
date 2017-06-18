3f0aa2bad0f30b6d082808b6a4af4b45c1ba027f
MBorkowski <mb.maciejborkowski@gmail.com>
2017-06-18 19:01:12 +0200

    skrol na menu, stylowanie strzałek

--- js/scripts.js
+++ js/scripts.js
@@ -1,15 +1,28 @@
 /* zwijanie/rozwijanie opisów prelegentów */
-function show_hide1 (obj) {
-  $(".hide").addClass("show");
-  $(".zwin").addClass("show_fa");
-  $(".rozwin").addClass("hide");
-};
 
-function show_hide2 () {
-  $(".hide").removeClass("show");
-  $(".zwin").removeClass("show_fa");
-  $(".rozwin").removeClass("hide");
-};
+$(document).ready(function(){
+    $("#myTopnav a").on('click', function(event) {
+        // Make sure this.hash has a value before overriding default behavior
+        if (this.hash !== "") {
+            // Prevent default anchor click behavior
+            event.preventDefault();
+
+            // Store hash
+            var hash = this.hash;
+
+            // Using jQuery's animate() method to add smooth page scroll
+            // The optional number (800) specifies the number of milliseconds it takes to scroll to the specified area
+            $('html, body').animate({
+                scrollTop: $(hash).offset().top
+            }, 800, function(){
+
+                // Add hash (#) to URL when done scrolling (default click behavior)
+                window.location.hash = hash;
+            });
+        }  // End if
+    });
+});
+
 
 
 /* pojawianie się opisów prelegentów na scroll */
